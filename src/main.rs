40d2d55f6292b83655d99aa154f8b3e9910e64cf
bwty <whalelephant@users.noreply.github.com>
2021-01-05 10:30:07 +0700

    Week 2

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use ff::Field;
 use ff::PrimeField;
 use rand_core::RngCore;
+use sha2::{Digest, Sha256};
+use std::convert::TryInto;
 
 /// This prime field has the greatest 128-bit prime as modulus. Because of the ff crate, each field
 /// element is 192bit (3*8 bytes) instead of 128 (2*8) bytes: take care when sampling random bytes.
@@ -19,7 +21,30 @@ impl FieldElement {
 		elm
 	}
 
-	/// Rejection samples random field element, we only sample 128bit since modulus is 128bit
+	fn hash(x: &str) -> Self {
+		let mut bytes = [0u8; 3 * 8];
+
+		let mut to_hash = x.as_bytes().to_vec();
+		let max_fill = 2 * 8;
+		loop {
+			let mut hasher = Sha256::new();
+			hasher.update(&to_hash[..]);
+			let hash: [u8; 32] = hasher
+				.finalize()
+				.as_slice()
+				.try_into()
+				.expect("Should be a 256-bit hash");
+			bytes[..max_fill].clone_from_slice(&hash[..max_fill]);
+
+			// Rejection Sampling
+			let repr = FieldElementRepr(bytes);
+			if let Some(e) = PrimeField::from_repr(repr) {
+				return e;
+			}
+			to_hash = hash.to_vec();
+		}
+	}
+
 	fn random() -> Self {
 		let mut rng = rand::thread_rng();
 		let mut bytes = [0u8; 3 * 8];
@@ -32,6 +57,77 @@ impl FieldElement {
 			}
 		}
 	}
+}
+
+/// Represents a Questionnair
+#[derive(Debug)]
+struct Questionnair {
+	questions: Vec<&'static str>,
+	tags: Vec<[u8; 32]>,
+	points: Vec<FieldElement>,
+}
+
+impl Questionnair {
+	/// Create random polynomial
+	/// Get Share
+	fn new(s: FieldElement, questions: Vec<&'static str>, answers: Vec<&'static str>) -> Self {
+		let degree = questions.len();
+		let polynomial = Polynomial::new(degree as u64, s);
+		let shares = polynomial.share(degree as u64);
+		let mut tags = Vec::new();
+		let mut points = Vec::new();
+
+		for ans in 0..degree {
+			let key = FieldElement::hash(&answers[ans]);
+			points.push(shares[ans].y + key);
+
+			let tag = tag_from_answer(answers[ans]);
+			tags.push(tag);
+		}
+		Questionnair {
+			questions,
+			tags,
+			points,
+		}
+	}
+}
+
+/// Generates Authenticity tag by H(H(a_i));
+fn tag_from_answer(ans: &'static str) -> [u8; 32] {
+	let mut hasher = Sha256::new();
+	hasher.update(ans);
+	let answer_hash = hasher.finalize_reset();
+	hasher.update(&answer_hash);
+	hasher
+		.finalize()
+		.as_slice()
+		.try_into()
+		.expect("Should be a 256-bit hash")
+}
+
+/// Lets user answer the questionnair
+/// First check if answers are correct
+/// Compute shares by calculating keys and decrypt points
+/// interpolation of shares to get secret
+fn answer(questionnair: Questionnair, answers: Vec<&'static str>) -> Result<FieldElement, String> {
+	let mut shares: Vec<Share> = Vec::with_capacity(answers.len());
+	// check each answer is
+	for (i, ans) in answers.iter().enumerate() {
+		let tag = tag_from_answer(*ans);
+		if tag != questionnair.tags[i] {
+			return Err("Wrong answer".to_string());
+		} else {
+			// key to decrypt points
+			let key = FieldElement::hash(ans);
+			shares.push(Share {
+				// x point starts at 1, not 0 as f(0) is the secret
+				x: FieldElement::new(i as u64 + 1),
+				y: questionnair.points[i] - key,
+			});
+		}
+	}
+	let interpolated = Polynomial::reconstruct(&shares);
+	Ok(interpolated)
 }
 
 /// Represents a polynomial over the finite field
@@ -96,29 +192,25 @@ impl Polynomial {
 			let mut n = FieldElement::one();
 			for j in 0..num_keys - 1 {
 				if i != j {
-					d = d * (-shares[j].x);
-					n = n * (shares[i].x - shares[j].x);
+					d *= -shares[j].x;
+					n *= shares[i].x - shares[j].x;
 				}
 			}
-			val = val + y * d * n.invert().unwrap();
+			val += y * d * n.invert().unwrap();
 		}
 		val
 	}
 }
 
 pub fn main() {
+	let answers = vec!["d", "e", "d", "e", "a"];
 	let secret = FieldElement::new(42);
 
-	println!("secret: {:?}", secret);
+	println!("Creating new Questionnair");
+	let questionair = Questionnair::new(secret, vec!["a", "b", "c", "b", "c"], answers.clone());
 
-	println!("Creating new polynomial");
-	let poly = Polynomial::new(5, secret.clone());
-
-	println!("Sharing polynomial");
-	let shares = poly.share(10);
-
-	println!("Reconstructing polynomial");
-	let interpolated = Polynomial::reconstruct(&shares[3..8]);
+	println!("Answering Questions");
+	let interpolated = answer(questionair, answers).unwrap();
 
 	println!("Checking secret is correct:");
 	println!(
